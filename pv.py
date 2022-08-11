d9afa40f60a7f6ebd862a283872552d1c54f27d5
Ben Vierck <ben@lit.ai>
2022-08-11 18:47:13 +0000

    comply with new schema

--- pv.py
+++ pv.py
@@ -8,10 +8,20 @@ def feature(adapter, index, vars=None, other_features=None):
 
     df = adapter.get_dataframe(lookback_index, index + 1)
 
-    feature.sample = df[['mdEntryPx', 'mdEntrySize']].astype(np.float32).values
+    feature.sample = df[['Price', 'Volume']].astype(np.float32).values
 
     price_offset = feature.sample[-1][0]
     feature.sample[:,0] = feature.sample[:,0] - price_offset
     return feature.sample[:]
     
 feature.sample = np.array((10, 2))
+
+def main():
+    from lit.data import loader
+    rds_path = "/opt/lit/refined_data/TSLA.O_2010.json"
+    adapter = loader.load_adapter(rds_path, limit=20000)
+    data = feature(adapter, 5000)
+    print(data)
+
+if __name__ == '__main__':
+    main()
