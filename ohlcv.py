22f7b19314c82b9f883ee4a8c2a78cbbf93ae940
Ben Vierck <ben@lit.ai>
2022-08-15 21:22:06 +0000

    init

---
+++ ohlcv.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+#TODO quadruple check for lookahead bias
+
+def feature(adapter, index, vars=None, other_features=None):
+    count = vars['count'] or 60
+    size = vars['size'] or 1
+    unit = vars['unit'] or 'sec'
+
+    df = adapter.get_dataframe(index, unit, (count * size) + 1)
+    price_offset = df.Price.iloc[-1]
+    df = df.set_index('Date-Time')
+    resample_unit = f"{size}{adapter.translate_resample_unit(unit)}"
+    
+    ohlc = df.resample(resample_unit)['Price'].ohlc().values
+    if len(ohlc) < count:
+        return []
+    ohlc -= price_offset
+    
+    vol = np.expand_dims(df.resample(resample_unit)['Volume'].sum().values, axis=1)
+    max_vol = np.expand_dims(df.resample(resample_unit)['Volume'].max().values, axis=1)
+
+    feature.sample = np.hstack([ohlc, vol, max_vol])[-count:]
+
+    return feature.sample[:]
+
+def main():
+    from lit.data import loader
+    rds = {
+        "adapter": { "name": "reuters_csv", "path": "/data/raw/test.csv", "resolution": 1 },
+        "features": [ { "count": 60, "size": 1, "unit": "sec" } ]
+    }
+    adapter = loader.load_adapter(json=rds, limit=20000)
+    data = feature(adapter, 5000, adapter.rds['features'][0])
+    print(data)
+
+if __name__ == '__main__':
+    main()
