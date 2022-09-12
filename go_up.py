fcd331744c4e57e5fb9b88b3b0b266162b703739
Ben Vierck <ben@lit.ai>
2022-09-12 22:35:11 +0000

    init risk-reward label

--- go_up.py
+++
@@ -1,9 +0,0 @@
-def feature(adapter, index, vars=None, other_features=None):
-    lookahead = vars['lookahead'] or 1024
-    df = adapter.get_dataframe(index, index + lookahead)
-    price_now = df.loc[index, 'mdEntryPx']
-    mean_future_price = df[['mdEntryPx']].mean()[0]
-    if mean_future_price > price_now:
-        return [1.0]
-    else:
-        return [0.0]
---
+++ risk_reward.py
@@ -0,0 +1,74 @@
+   
+def feature(adapter, index, vars=None, other_features=None):
+    
+    risk = vars['risk']
+    reward = vars['reward']
+    type = vars['type']
+
+    count = vars['count'] or 60
+    size = vars['size'] or 1
+    unit = vars['unit'] or 'sec'
+
+    data = adapter.get_bars(index, count, unit, size, future=True)
+    if len(data) != count:
+        return []    
+
+    start_price = data[0, 0]
+    if type == "long":
+        loss_price = start_price - risk
+        win_price = start_price + reward
+        loss_comparator = lambda low, high: low <= loss_price
+        win_comparator = lambda low, high: high >= win_price
+    elif type == "short":
+        loss_price = start_price + risk
+        win_price = start_price - reward
+        loss_comparator = lambda low, high: high >= loss_price
+        win_comparator = lambda low, high: low <= win_price
+    else:
+        raise Exception("Invalid type input; must be either 'long' or 'short'")
+
+    for index in range(len(data)):
+        low, high = data[index,2], data[index,1]
+        if loss_comparator(low, high):
+            return [0.0]
+        if win_comparator(low, high):
+            return [1.0]
+
+    return [0.0] # no partial wins
+
+def interrogate():
+    return [
+        { 
+            "name": "lookahead", 
+            "description": "How many ticks into the future to look ahead",
+            "type": "number"
+        },
+        {
+            "name": "another property name",
+            "description": "another description",
+            "type": "boolean"
+        }
+    ]
+
+
+def main():
+    from lit.data import loader
+    import h5py
+    from random import randint
+
+    rds = {
+        "adapter": { "name": "reuters", "path": "/data/raw/TSLA.O_ALL.csv" },
+        "features": [ { "risk": 1, "reward": 1, "type": "long", "count": 10, "size": 1, "unit": "day" } ]
+    }
+    adapter = loader.load_adapter(json=rds)
+
+    with h5py.File("/data/raw/TSLA.O_ALL.h5", "r") as f:
+        sample_count = f['timestamp'].shape[0]
+
+    indexes = [randint(1, sample_count) for i in range(10)]
+    for index in indexes:
+        data = len(feature(adapter, index, adapter.rds['features'][0]))
+
+
+if __name__ == '__main__':
+    main()
